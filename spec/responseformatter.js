531ecc5369eb302c3116e66036940e57306bbbe1
Steven Li <stevenli@Stevens-MacBook-Pro.local>
2017-11-28 10:03:18 -0800

    responseformatter

---
+++ spec/responseformatter.js
@@ -0,0 +1,13 @@
+class ResponseFormatter {
+  constructor() {}
+
+  //takes the return item from commandrunner.js
+}
+
+/*should return
+`
+griselde's public repos
+Code-Coven - A version control system for weird sisters
+Eye-Of-Newt - EON is a new frontend framework written in pure spaghetti code.
+`
+*/
--- src/commandrunner.js
+++ src/commandrunner.js
@@ -1,6 +1,87 @@
+let mockGithubObject = [
+  {
+    repos: [
+      {
+        name: "repo1name",
+        description: "cool project"
+      },
+      {
+        name: "repo2name",
+        description: "cooler project"
+      },
+      {
+        name: "repo3name",
+        description: "coolest project"
+      }
+    ],
+
+    starredrepos: [
+      {
+        name: "repo3name",
+        description: "coolest project"
+      }
+    ],
+
+    name: "griselda"
+  },
+
+  {
+    repos: [
+      {
+        name: "repo1name",
+        description: "awesome project"
+      },
+      {
+        name: "repo2name",
+        description: "awesomer project"
+      },
+      {
+        name: "repo3name",
+        description: "awesomest project"
+      }
+    ],
+
+    starredrepos: [
+      {
+        name: "repo3name",
+        description: "awesomest project"
+      }
+    ],
+
+    name: "brunhilde55"
+  }
+];
+
 class CommandRunner {
-	constructor() {}
+  constructor() {}
 
+  responsify(input) {
+    //input is an object from questionparser.parser {username: , subject: }
+    let response = {};
 
+    //adding old keys to response
+    response.username = input.username;
+    response.subject = input.subject;
+    response.query = input.query;
 
+    //finding the results => first get the right object from mockGithubObject
+    let rightObj = mockGithubObject.filter(
+      obj => obj.name === response.username
+    );
+
+    //then get the starredrepos or repo from mockGithubObject
+    let subject = response.subject.split(" ").join("");
+    let rightItem = rightObj[subject];
+
+    let queries = {
+      count: rightItem.length,
+      details: rightItem
+    };
+
+    response.results = queries[response.query];
+
+    return response;
+  }
 }
+
+module.exports = CommandRunner;
